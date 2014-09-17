c4d7fb164b17d4e5208b431636937e04959456bb
hortinstein <hortinstein@gmail.com>
2014-09-17 15:21:14 +0000

    added makefile

--- 6300Fall14Project1/src/edu/gatech/seclass/project1/ASL.java
+++ 6300Fall14Project1/src/edu/gatech/seclass/project1/ASL.java
@@ -1,7 +1,7 @@
 package edu.gatech.seclass.project1;
 
 public class ASL {
-
+	
 	//for the we-can't-change-them tests, this needs to implement:
 	//void setFile(File theFile)
 	//int computeAverageSentenceLength()
