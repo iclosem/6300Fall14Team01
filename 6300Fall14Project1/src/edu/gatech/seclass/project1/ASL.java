52967a6058de04d6e8867317e744fb49a1f1aef0
Alex Hortin <hortinstein@gmail.com>
2014-09-17 15:20:53 -0400

    spacing

--- 6300Fall14Project1/src/edu/gatech/seclass/project1/ASL.java
+++ 6300Fall14Project1/src/edu/gatech/seclass/project1/ASL.java
@@ -12,29 +12,24 @@ import java.nio.file.Paths;
 
 
 public class ASL {
-    
-
 	private String delimiters = "[.?!]"; //| F_04.1 | System default for sentence delimiters will be .?!; || high
-    private String essay;
-    private int minWordLength = 3; //|F_03.1 | System default for minimum character length defining a word will be set to three |  | high
-    private int averageSentanceLength;
-    
-
+	private String essay;
+	private int minWordLength = 3; //|F_03.1 | System default for minimum character length defining a word will be set to three |  | high
+	private int averageSentanceLength;
 	private int computeAverageSentenceLength(){
         String[] essaySentances = essay.split(this.delimiters, 0);//split into sentances, 0 means for all instances
         int totalChars = 0; //keeping track of total characters
        	int totalSentances = essaySentances.length; // -1 since split retains the first sentance
         //| F_02.0 | System will output the average (mean) number of words per sentence, rounded down to the nearest integer | | high
         for (String strings : essaySentances) { // for all of the sentances in essay
-            if(strings.length()>0){ //eliminates zero element items resulting from elipses etc and min ammount of chars
-                ///NEED TO CHECK MINIMUM WORD VALUE TO COUND 
-                //possible implementation strings.split(" ",0) > self.minLength
-                
-                totalChars+=strings.length(); //adding current sentance 
-                //System.out.println(strings + ", "+ strings.length()); //debugging print statement
-            } else { //if there is a zero element sentance, it skips it and decrements
-                totalSentances--;
-            }
+	        if(strings.length()>0){ //eliminates zero element items resulting from elipses etc and min ammount of chars
+	        	///NEED TO CHECK MINIMUM WORD VALUE TO COUND 
+		        //possible implementation strings.split(" ",0) > self.minLength
+		        totalChars+=strings.length(); //adding current sentance 
+		        //System.out.println(strings + ", "+ strings.length()); //debugging print statement
+		      } else { //if there is a zero element sentance, it skips it and decrements
+		        totalSentances--;
+		      }
         }
         //TODO
         //
@@ -70,10 +65,10 @@ public class ASL {
 	
     
     void readFile(String filePath){
-        try{
-        	String essay = new String(Files.readAllBytes(Paths.get(filePath))); // reads the bytes in from the file
-        } catch(java.io.IOException e){//| F_1.02 | System will produce a friendly feedback message to user if file path input is not successful in loading file. "The file path that has been input was not successful, please re-enter file path.” | | medium
- 			System.out.println("The file path that has been input was not successful, please re-enter file path.");//F_01.02, should throw exception or report error if file does not exist
+    	try{
+    		String essay = new String(Files.readAllBytes(Paths.get(filePath))); // reads the bytes in from the file
+    	} catch(java.io.IOException e){//| F_1.02 | System will produce a friendly feedback message to user if file path input is not successful in loading file. "The file path that has been input was not successful, please re-enter file path.” | | medium
+    	System.out.println("The file path that has been input was not successful, please re-enter file path.");//F_01.02, should throw exception or report error if file does not exist
 		}
         
         //TODO
