dcb6e1fff38fc6fadb4dd7bc841fe5342f82a8ae
bowersaa <alexandra.bowers@gmail.com>
2014-09-16 20:22:25 -0400

    Adding base tests, minimum implemented fns

--- 6300Fall14Project1/src/edu/gatech/seclass/project1/ASL.java
+++ 6300Fall14Project1/src/edu/gatech/seclass/project1/ASL.java
@@ -2,4 +2,10 @@ package edu.gatech.seclass.project1;
 
 public class ASL {
 
+	//for the we-can't-change-them tests, this needs to implement:
+	//void setFile(File theFile)
+	//int computeAverageSentenceLength()
+	//void setSentenceDelimiters(String delimiters)
+	//void setMinWordLength(int minLength)
+	
 }
--- 6300Fall14Project1/test/edu/gatech/seclass/project1/ASLTest.java
+++ 6300Fall14Project1/test/edu/gatech/seclass/project1/ASLTest.java
@@ -2,13 +2,47 @@ package edu.gatech.seclass.project1;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class ASLTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    private ASL asl;
+    private String fileDir;
 
+    @Before
+    public void setUp() throws Exception {
+        asl = new ASL();
+        fileDir = new String("test" + File.separator + "inputfiles"
+                + File.separator);
+    }
+    @After
+    public void tearDown() throws Exception {
+        asl = null;
+        fileDir = null;
+    }
+    @Test
+    public void testComputeAverageSentenceLength1() {
+        String comment = "Testing sentences that span multiple lines";
+        asl.setFile(new File(fileDir + "input.txt"));
+        assertEquals(comment, 7, asl.computeAverageSentenceLength(), 0);
+    }
+    @Test
+    public void testComputeAverageSentenceLength2() {
+        String comment = "Testing customized delimiters";
+        asl.setFile(new File(fileDir + "input.txt"));
+        asl.setSentenceDelimiters("%.");
+        assertEquals(comment, 3, asl.computeAverageSentenceLength(), 0);
+    }
+    @Test
+    public void testComputeAverageSentenceLength3() {
+        String comment = "Testing customized minimal word length";
+        asl.setFile(new File(fileDir + "input.txt"));
+        asl.setMinWordLength(5);
+        assertEquals(comment, 3, asl.computeAverageSentenceLength(), 0);
+    }
 }
