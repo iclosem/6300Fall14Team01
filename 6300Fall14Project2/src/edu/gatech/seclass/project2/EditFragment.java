58106f8ed971e39aad86ad6298b98e30a601f685
Alex Hortin <hortinstein@gmail.com>
2014-10-28 00:25:19 -0400

    tabs are fully switching with fragments now!
    hurray!

---
+++ 6300Fall14Project2/src/edu/gatech/seclass/project2/EditFragment.java
@@ -0,0 +1,20 @@
+package edu.gatech.seclass.project2;
+
+import edu.gatech.seclass.project2.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class EditFragment extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_edit, container, false);
+         
+        return rootView;
+    }
+}
