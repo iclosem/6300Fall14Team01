1aa28e61ab585845129690cbbf7f3ad785f59284
bowersaa <alexandra.bowers@gmail.com>
2014-11-03 18:13:17 -0500

    Why won't genymotion update?

--- 6300Fall14Project2/src/edu/gatech/seclass/project2/CreateActivity.java
+++ 6300Fall14Project2/src/edu/gatech/seclass/project2/CreateActivity.java
@@ -2,11 +2,8 @@ package edu.gatech.seclass.project2;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.RadioButton;
 
 public class CreateActivity extends Activity {
 
@@ -15,35 +12,23 @@ public class CreateActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.create, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 	
 	public void handleClick(View view){
 
 		EditText txtName = (EditText) findViewById(R.id.EditTextName);
 		EditText txtAddr = (EditText) findViewById(R.id.EditTextAddress);
 		EditText txtDob = (EditText) findViewById(R.id.EditTextDOB);
-		if(!txtName.getText().toString().matches("") && !txtAddr.getText().toString().matches("") && !txtDob.getText().toString().matches("")){
-			Customer newCust = new Customer(txtName.getText().toString(), txtAddr.getText().toString(), txtDob.getText().toString() );
-			EditText txtVipNum = (EditText) findViewById(R.id.NewCreatedVIPNum);
-			txtVipNum.setText(newCust.getVIPNumber());
+		EditText txtVipNum = (EditText) findViewById(R.id.NewCreatedVIPNum);
+		try{
+			if(!txtName.getText().toString().matches("") && !txtAddr.getText().toString().matches("") && !txtDob.getText().toString().matches("")){
+				Customer newCust = new Customer(txtName.getText().toString(), txtAddr.getText().toString(), txtDob.getText().toString() );
+				txtVipNum.setText(newCust.getVIPNumber());
+			}else{
+				txtVipNum.setText("Wrong");
+			}
+		}catch(Exception e){
+			txtVipNum.setText("FAIL");
 		}
+		
 	}
 }
