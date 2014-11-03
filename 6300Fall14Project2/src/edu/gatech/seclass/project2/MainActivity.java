6278eb50c978e54a24e3b4d2802b902b9c9aab7e
bowersaa <alexandra.bowers@gmail.com>
2014-11-03 14:58:45 -0500

    Regenerate R.java

--- 6300Fall14Project2/src/edu/gatech/seclass/project2/MainActivity.java
+++ 6300Fall14Project2/src/edu/gatech/seclass/project2/MainActivity.java
@@ -1,88 +1,48 @@
 package edu.gatech.seclass.project2;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBar.Tab;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar.TabListener;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.view.*;
 
-public class MainActivity extends FragmentActivity implements android.app.ActionBar.TabListener {
+public class MainActivity extends Activity {
 	   	 
-	private ViewPager viewPager;
-	private TabsPagerAdapter mAdapter;
-	private android.app.ActionBar actionBar;
 	// Tab titles
 	private String[] tabs = { "Sell", "Preorder", "VIPS", "Report", "Create" };
 	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.activity_main);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 	
-	    // Initialization
-	    viewPager = (ViewPager) findViewById(R.id.pager);
-	    actionBar = this.getActionBar();
-	    mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
-	
-	    viewPager.setAdapter(mAdapter);
-	    actionBar.setHomeButtonEnabled(false);
-	    actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);        
-	
-	    // Adding Tabs
-	    for (String tab_name : tabs) {
-	        actionBar.addTab(actionBar.newTab().setText(tab_name).setTabListener(this));
-	    }
-	
-	    /**
-	     * on swiping the viewpager make respective tab selected
-	     * */
-	    viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-	
-	        @Override
-	        public void onPageSelected(int position) {
-	            // on changing the page
-	            // make respected tab selected
-	            actionBar.setSelectedNavigationItem(position);
-	        }
-	
-	        @Override
-	        public void onPageScrolled(int arg0, float arg1, int arg2) {
-	        }
-	
-	        @Override
-	        public void onPageScrollStateChanged(int arg0) {
-	        }
-	    });
+	public void startNewActivity(Class activityClass ){	
+
+		Intent intent = new Intent(this, activityClass);
+		startActivity(intent);
 	}
 
-	@Override
-	public void onTabSelected(android.app.ActionBar.Tab tab,
-			android.app.FragmentTransaction ft) {
-		 // on tab selected
-        // show respected fragment view
-        viewPager.setCurrentItem(tab.getPosition());
-		
-	}
-
-	@Override
-	public void onTabUnselected(android.app.ActionBar.Tab tab,
-			android.app.FragmentTransaction ft) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onTabReselected(android.app.ActionBar.Tab tab,
-			android.app.FragmentTransaction ft) {
-		// TODO Auto-generated method stub
-		
+	public void handleMenuSelection(View view){
+				
+		switch(view.getId()){
+		case R.id.button1:
+			startNewActivity( CreateActivity.class);
+			break;
+		case R.id.button2:
+			startNewActivity(VIPActivity.class);
+			break;
+		case R.id.button3:
+			startNewActivity(SellActivity.class);
+			break;
+		case R.id.button4:
+			startNewActivity(PreorderActivity.class);
+			break;
+		case R.id.button5:
+			startNewActivity(ReportActivity.class);
+			break;
+			
+		}
 	}
 	
 
--- 6300Fall14Project2/src/edu/gatech/seclass/project2/VIPFragment.java
+++ 6300Fall14Project2/src/edu/gatech/seclass/project2/VIPFragment.java
@@ -1,6 +1,6 @@
 package edu.gatech.seclass.project2;
 
-import edu.gatech.seclass.project2.R;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
