7ab5fd51771628b9a483d442eb1c14955e679e63
Alex Hortin <hortinstein@gmail.com>
2014-11-04 21:19:44 -0500

    added old fragment code

---
+++ 6300Fall14Project2/legacy/SellFragment.java
@@ -0,0 +1,20 @@
+package edu.gatech.seclass.project2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+ 
+public class SellFragment extends Fragment {
+ 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_sell, container, false);
+         
+        return rootView;
+    }
+}
